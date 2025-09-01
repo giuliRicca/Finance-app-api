1770f5e9fe21574cfbfcc3586c8e2d22bf1c42d3
GiuilanoRicca <guilianor@wilson-franco.com>
2025-09-01 18:09:11 -0300

    Create .net project and add first entities and migrations

---
+++ Finance-Api/Core/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Finance_Api.Core.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; } = String.Empty;
+        public string Email { get; set; } = String.Empty;
+        public string PasswordHash { get; set; } = String.Empty;
+    }
+}
---
+++ Finance-Api/Infrastructure/Data/FinanceTrackerDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Finance_Api.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Finance_Api.Infrastructure.Data
+{
+    public class FinanceTrackerDbContext : DbContext
+    {
+        public FinanceTrackerDbContext(DbContextOptions<FinanceTrackerDbContext> options)
+            : base(options)
+        {
+        }
+
+        // DbSets
+        public DbSet<User> Users { get; set; } = null!;
+    }
+}
